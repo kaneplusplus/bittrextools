924473a4edb3d7c4db67299bca5e47375a046a94
Michael Kane <kaneplusplus@gmail.com>
2017-11-05 10:58:05 -0500

    Market flipping.

--- R/util.r
+++ R/util.r
@@ -138,6 +138,88 @@ execute_sell <- function(market, quantity, rate, dry_run=TRUE, log_cb=NULL) {
   invisible(TRUE)
 }
 
+#' Flip the base and quote currency for a market
+#'
+#' @param x the vector of markets to flip.
+#' @export
+flip_market_name <- function(x) {
+  if (length(x)) {
+    mkts <- matrix(unlist(strsplit(x, "-")), ncol=2, byrow=TRUE)
+    apply(mkts, 1, function(s) paste0(s[2], "-", s[1]))
+  } else {
+    character()
+  }
+}
+
+
+#' Flip a market 
+#'
+#' @param market_name the market names.
+#' @param high the vector of highs.
+#' @param low the vector of lows. 
+#' @param volume the vector of volumes. 
+#' @param last the vector of last prices.
+#' @param base_volume the vector of base_volumes - this gets set to NA.
+#' @param time_stamp the vector of time stamps.
+#' @param bid the vecor of bids.
+#' @param ask the vecor of asks. 
+#' @param open_buy_orders the vector of open buy orders.
+#' @param open_sell_orders the vector of open sell orders.
+#' @param prev_day the previous day's close.
+#' @param created the vector of dates the market was created.
+#' @export
+flip_markets <- function(market_name, high, low, volume, last, base_volume,
+  time_stamp, bid, ask, open_buy_orders, open_sell_orders, prev_day, created) {
+  ret <- tibble(market_name=flip_market_name(market_name))
+  if (!missing(high)) {
+    ret$low <- 1/high
+  }
+  if (!missing(low)) {
+    ret$low <- 1/high
+  }
+  if (!missing(volume)) {
+    ret$volume <- volume
+  }
+  if (!missing(last)) {
+    ret$last <- 1/last
+  }
+  if (!missing(base_volume)) {
+    ret$base_volume <- NA
+  }
+  if (!missing(time_stamp)) {
+    ret$time_stamp <- time_stamp
+  }
+  if (!missing(ask)) {
+    ret$bid <- 1/ask
+  }
+  if (!missing(bid)) {
+    ret$ask <- 1/bid
+  }
+  if (!missing(open_sell_orders)) {
+    ret$open_buy_orders <- open_sell_orders
+  }
+  if (!missing(open_buy_orders)) {
+    ret$open_sell_orders <- open_buy_orders
+  }
+  if (!missing(prev_day)) {
+    ret$prev_day <- 1/prev_day
+  }
+  if (!missing(created)) {
+    ret$prev_day <- prev_day
+  }
+  ret
+}
+
+#' Flip Bittrex market summaries
+#'
+#' @param x the market summary
+#' @export
+flip_market_summary <- function(x) {
+  flip_markets(x$market_name, x$high, x$low, x$volume, x$last, x$base_volume,
+    x$time_stamp, x$bid, x$ask, x$open_buy_orders, x$open_sell_orders,
+    x$prev_day, x$created)
+}
+
 #' If a bittrex call was successful, return the data
 #'
 #' @param x the return of a call to the Bittrex exchange 
